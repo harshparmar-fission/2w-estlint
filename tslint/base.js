a0563a040a74c92db9308426f3f8808fce596372
Steve Byerly <steven.j.byerly@gmail.com>
2017-04-28 10:58:56 -0700

    update packages. add new tslint style package (#9)

--- tslint/base.js
+++ tslint/base.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
-const tslintMS = require.resolve('tslint-microsoft-contrib');
+const microsoft = require.resolve('tslint-microsoft-contrib');
+const codestyle = require.resolve('tslint-consistent-codestyle/rules');
 
 module.exports = {
   extends: [
     'tslint:latest',
     'tslint-eslint-rules',
+    './rules/codestyle.js',
     './rules/eslint.js',
     './rules/functionality.js',
     './rules/maintainability.js',
@@ -14,6 +16,7 @@ module.exports = {
     './rules/typescript.js',
   ],
   rulesDirectory: [
-    path.dirname(tslintMS),
+    path.dirname(microsoft),
+    path.dirname(codestyle),
   ],
 };
--- tslint/rules/react.js
+++ tslint/rules/react.js
@@ -1,6 +1,13 @@
 module.exports = {
   rules: {
-    'jsx-boolean-value': false,
+    'jsx-alignment': true,
+    'jsx-boolean-value': [true, 'never'],
+    'jsx-curly-spacing': [true, 'never'],
+    'jsx-no-bind': true,
+    'jsx-no-lambda': true,
     'jsx-no-multiline-js': false,
+    'jsx-no-string-ref': true,
+    'jsx-self-close': true,
+    'jsx-wrap-multiline': true,
   },
 };
