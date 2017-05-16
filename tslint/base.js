d3b6aa06f7d66196d6b697828809b2dc563c47a2
Steve Byerly <steven.j.byerly@gmail.com>
2017-05-16 14:21:32 -0700

    Feature/greenkeeping (#10)
    * upgrade linters. add more microsoft rules
    
    * Add peer dep

--- tslint/base.js
+++ tslint/base.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
 const microsoft = require.resolve('tslint-microsoft-contrib');
-const codestyle = require.resolve('tslint-consistent-codestyle/rules');
 
 module.exports = {
   extends: [
     'tslint:latest',
     'tslint-eslint-rules',
+    'tslint-consistent-codestyle',
     './rules/codestyle.js',
     './rules/eslint.js',
     './rules/functionality.js',
     './rules/maintainability.js',
+    './rules/microsoft.js',
     './rules/react.js',
     './rules/style.js',
     './rules/typescript.js',
   ],
   rulesDirectory: [
     path.dirname(microsoft),
-    path.dirname(codestyle),
   ],
 };
